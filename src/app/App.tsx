fd9da9f7615ca4f22533700e1f9e6729b52b8ab5
Simona <86434048+Sajmon24@users.noreply.github.com>
2024-11-12 23:15:32 +0100

    feat: add auth layout, login & sign-up ui with react hook forms (#2)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,24 @@
-import { AppRouter } from "@config/routes";
-import { theme } from "@config/styles";
-import { CssBaseline } from "@mui/material";
-import { ThemeProvider } from "@mui/material/styles";
+import { SnackbarProvider } from 'notistack';
+import { BrowserRouter } from 'react-router-dom';
+
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+
+import { AppRouter } from '@config/routes';
+import { theme } from '@config/styles';
+import { useAuthStateSubscription } from '@services/firebase';
 
 export default function App() {
+  useAuthStateSubscription();
+
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <AppRouter />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <SnackbarProvider>
+          <AppRouter />
+        </SnackbarProvider>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,18 @@
-import { Typography } from "@mui/material";
-import Button from "@mui/material/Button";
+import { Typography } from '@mui/material';
+
+import { APP_NAME } from '@config/constants';
+import LoginForm from '@features/auth/components/LoginForm';
 
 export default function LoginPage() {
   return (
-    <div>
-      <Typography variant="h1">Login</Typography>
-      <Button variant="contained">Hello world</Button>; login
-    </div>
+    <>
+      <Typography component="h1" variant="h2" mb={1}>
+        Login
+      </Typography>
+      <Typography color="text.secondary" mb={4}>
+        Login to access {APP_NAME}
+      </Typography>
+      <LoginForm />
+    </>
   );
 }
