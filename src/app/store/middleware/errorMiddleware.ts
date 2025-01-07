3bebc36c274468b0b2c3b866d9a10dc7267dd249
Sajmon24 <fickova.simona@outlook.sk>
2025-01-07 13:21:25 +0100

    feat: update trips api with tags for cache invalidation

--- src/app/store/middleware/errorMiddleware.ts
+++ src/app/store/middleware/errorMiddleware.ts
@@ -9,6 +9,22 @@ export const rtkQueryErrorLogger: Middleware = () => (next) => (action) => {
       variant: 'error',
     });
   }
-
   return next(action);
 };
+// import { enqueueSnackbar } from 'notistack';
+
+// import { isRejectedWithValue } from '@reduxjs/toolkit';
+// import type { Middleware } from '@reduxjs/toolkit';
+
+// export const rtkQueryErrorLogger: Middleware = () => (next) => (action) => {
+//   if (isRejectedWithValue(action) || 'error' in action) {
+//     enqueueSnackbar(
+//       action.payload ?? action.error?.message ?? 'Something went wrong!',
+//       {
+//         variant: 'error',
+//       },
+//     );
+//   }
+
+//   return next(action);
+// };
